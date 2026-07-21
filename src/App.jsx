1e05802baa29fa0e30b6814b87a59666e3d3c6ee
Richo Ha <richard.dqh@gmail.com>
2026-07-21 20:24:33 +0800

    Add controlled height input

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import LabelPreview from "./component/LabelPreview";
 export default function App() {
   const [holeCount, setHoleCount] = useState(4);
   const [rectWidth, setRectWidth] = useState(10);
+  const [rectHeight, setRectHeight] = useState(5);
 
   const StartRectX = 45;
   const StartRectY = 2.5;
-  const rectHeight = 5;
   const fontSize = 3;
 
   const holeR = 0.5;
@@ -43,6 +43,11 @@ export default function App() {
         type="number"
         value={rectWidth}
         onChange={(e) => setRectWidth(parseFloat(e.target.value))}
+      />
+      <input
+        type="number"
+        value={rectHeight}
+        onChange={(e) => setRectHeight(parseFloat(e.target.value))}
       />
       <LabelPreview
         rectWidth={rectWidth}
